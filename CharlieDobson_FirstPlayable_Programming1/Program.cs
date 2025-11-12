fca6ad399b2f480d33e1a84693c9c3c2984cd2ff
C-DGameMaker <charlotte5634@outlook.com>
2025-11-12 10:36:39 -0400

    Added Temp map and border

--- CharlieDobson_FirstPlayable_Programming1/Program.cs
+++ CharlieDobson_FirstPlayable_Programming1/Program.cs
@@ -21,8 +21,32 @@ namespace CharlieDobson_FirstPlayable_Programming1
         static int totalEnemyKilled;
 
         //Map
+        // ░ = grass
+        // ▒ = water
+        // ▓ = mountain
+        // █ = trees
+        // ═ = border top
+        // ║ = border line
+        // ╔ ╚ ╗ ╝ = Corner
 
-        
+        static string[,] map =
+        {
+            {"▓", "▓", "▓", "░", "░", "█", "█", "█", "█", "░", "░", "░", "█", "█", "█", "█", "█"},
+            {"░", "▓", "░", "░", "█", "█", "▒", "█", "░", "░", "░", "░", "░", "█", "█", "█", "░"},
+            {"█", "░", "░", "░", "█", "▒", "▒", "█", "░", "░", "░", "░", "░", "█", "█", "░", "░"},
+            {"█", "█", "░", "░", "█", "▒", "▒", "█", "█", "█", "░", "░", "░", "░", "░", "░", "░"},
+            {"█", "█", "█", "░", "█", "█", "▒", "▒", "▒", "█", "░", "░", "░", "░", "░", "░", "░"},
+            {"▒", "▒", "▒", "░", "░", "█", "█", "█", "▒", "█", "░", "░", "░", "░", "░", "░", "░"},
+            {"▒", "▒", "▒", "▒", "░", "░", "█", "█", "█", "█", "░", "░", "░", "░", "▒", "▒", "▒"},
+            {"█", "█", "█", "░", "░", "░", "░", "█", "█", "░", "░", "░", "░", "░", "▒", "▒", "▒"},
+            {"█", "█", "░", "░", "░", "░", "░", "░", "█", "░", "░", "░", "░", "░", "▒", "▒", "▓"},
+            {"▓", "█", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "▓", "▓", "▓"},
+            {"▓", "▓", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "▓", "▓", "▓", "▓"},
+            {"▓", "▓", "▓", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "▓", "▓", "▓"},
+            {"▓", "▓", "▓", "▓", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "▓", "▓"},
+        };
+
+
 
 
         static void Main(string[] args)
@@ -30,24 +54,121 @@ namespace CharlieDobson_FirstPlayable_Programming1
             Intro();
             while(isAlive == true)
             {
-
+                Draw();
+                Console.ReadKey(true);
             }
             Ending();
         }
 
         static void Intro()
         {
-            
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Welcome to my firs playable program.");
+            Console.ResetColor();
+
+            Console.WriteLine("Use WSAD to walk.");
+            Console.WriteLine("If you walk into an enemy, you will damage it.");
+            Console.WriteLine("If an enemy walks into you, it will damage you.");
+
+            Console.ReadKey(true);
+            Console.Clear();
+
         }
+
 
         static void Ending()
         {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("YOU DIED");
+            Console.ResetColor();
 
+            Console.WriteLine($"You killed {totalEnemyKilled} number of enemies.");
+            Console.WriteLine("RESET PRORGAM TO TRY AGAIN");
+            Console.ReadKey(true);
+            Console.Clear();
         }
 
         static void MakeMap(int scale)
         {
+            for (int b = 0; b < map.GetLength(1) * scale + 2; b++)
+            {
+                if(b == 0)
+                {
+                    Console.Write("╔");
+                }
+                if(b == map.GetLength(1) * scale + 1)
+                {
+                    Console.Write("╗");
+                }
 
+                Console.Write("═");
+            }
+            Console.Write("\n");
+
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int s = 0; s < scale; s++)
+                {
+                    Console.Write("║");
+                    for (int x = 0; x < map.GetLength(1); x++)
+                    {
+                        for (int i = 0; i < scale; i++)
+                        {
+                            if (map[y, x] == "▒")
+                            {
+                                Console.BackgroundColor = ConsoleColor.Blue;
+                            }
+                            else if (map[y, x] == "░")
+                            {
+                                Console.BackgroundColor = ConsoleColor.Green;
+                            }
+                            else if (map[y, x] == "▓")
+                            {
+                                Console.BackgroundColor = ConsoleColor.Gray;
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                            }
+                            else if (map[y, x] == "█")
+                            {
+                                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            }
+
+                            Console.Write(map[y, x]);
+                            Console.ResetColor();
+                        }
+
+                    }
+                    Console.Write("║");
+                    Console.Write("\n");
+                }
+
+            }
+
+            for (int b = 0; b < map.GetLength(1) * scale + 2; b++)
+            {
+                if (b == 0)
+                {
+                    Console.Write("╚");
+                }
+                if (b == map.GetLength(1) * scale + 1)
+                {
+                    Console.Write("╝");
+                }
+                Console.Write("═");
+            }
+            Console.Write("\n");
         }
+
+        static void HUD()
+        {
+            
+        }
+
+        static void Draw()
+        {
+            MakeMap(1);
+        }
+
+        
     }
 }
