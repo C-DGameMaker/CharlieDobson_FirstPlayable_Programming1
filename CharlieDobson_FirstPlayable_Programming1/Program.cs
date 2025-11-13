846c8451736690361a70bbd65b3cd57bea34b624
C-DGameMaker <charlotte5634@outlook.com>
2025-11-13 13:08:32 -0400

    Added Gameplay loop start + other stuff
    Added method for gameplay loop
    Added more to the HUD
    Added Player Character(No movement yet)
    Added Game Loading animation at the start (though its commented out for now for game loop stuff)
    Added Functional Health status system

--- CharlieDobson_FirstPlayable_Programming1/Program.cs
+++ CharlieDobson_FirstPlayable_Programming1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CharlieDobson_FirstPlayable_Programming1
@@ -10,6 +11,7 @@ namespace CharlieDobson_FirstPlayable_Programming1
     {
         //Player
         static int playerHealth = 100;
+        static string playerStatus = "";
         static int curGold;
         static bool isAlive = true;
 
@@ -25,9 +27,6 @@ namespace CharlieDobson_FirstPlayable_Programming1
         // ▒ = water
         // ▓ = mountain
         // █ = trees
-        // ═ = border top
-        // ║ = border line
-        // ╔ ╚ ╗ ╝ = Corner
 
         static string[,] map =
         {
@@ -46,21 +45,49 @@ namespace CharlieDobson_FirstPlayable_Programming1
             {"▓", "▓", "▓", "▓", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "▓", "▓"},
         };
 
+        //Movement
+
+        static int xAxisPlayer = 20;
+        static int yAxisPlayer = 17;
+
+        static int xAxisEnemy;
+        static int yAxisEnemy;
+
 
 
 
         static void Main(string[] args)
         {
-            Console.CursorVisible = false;
+            //Console.CursorVisible = false;
             //Intro();
-            while(isAlive == true)
+            //Console.ReadKey(true);
+            //Console.Clear();
+
+            //StartGame();
+            //Console.ReadKey(true);
+            //Console.Clear();
+            while (isAlive == true)
             {
+                ProcessInput();
+                Update();
                 Draw();
                 Console.ReadKey(true);
+                Console.Clear();
+                
             }
             Ending();
         }
 
+        static void StartGame()
+        {
+            Console.Write("Loading map");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            MakeMapThread(2);
+            Console.WriteLine();
+            Console.WriteLine("Press any key to start");
+        }
         static void Intro()
         {
             Console.ForegroundColor = ConsoleColor.Magenta;
@@ -70,9 +97,6 @@ namespace CharlieDobson_FirstPlayable_Programming1
             Console.WriteLine("Use WSAD to walk.");
             Console.WriteLine("If you walk into an enemy, you will damage it.");
             Console.WriteLine("If an enemy walks into you, it will damage you.");
-
-            Console.ReadKey(true);
-            Console.Clear();
 
         }
 
@@ -176,20 +200,180 @@ namespace CharlieDobson_FirstPlayable_Programming1
             }
         }
 
+        static void MakeMapThread(int scale)
+        {
+            for (int b = 0; b < map.GetLength(1) * scale + 2; b++)
+            {
+                if (b == 0)
+                {
+                    Console.Write("╔");
+                }
+
+                else if (b == map.GetLength(1) * scale + 1)
+                {
+                    Console.Write("╗");
+                }
+
+                else if (b > map.GetLength(1) * scale + 1)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("═");
+                }
+            }
+            Thread.Sleep(100);
+            Console.Write("\n");
+
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int s = 0; s < scale; s++)
+                {
+                    Console.Write("║");
+                    for (int x = 0; x < map.GetLength(1); x++)
+                    {
+                        for (int i = 0; i < scale; i++)
+                        {
+                            if (map[y, x] == "▒")
+                            {
+                                Console.BackgroundColor = ConsoleColor.Blue;
+                            }
+                            else if (map[y, x] == "░")
+                            {
+                                Console.BackgroundColor = ConsoleColor.Green;
+                            }
+                            else if (map[y, x] == "▓")
+                            {
+                                Console.BackgroundColor = ConsoleColor.Gray;
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                            }
+                            else if (map[y, x] == "█")
+                            {
+                                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            }
+
+                            Console.Write(map[y, x]);
+                            Console.ResetColor();
+                        }
+
+                    }
+                    Console.Write("║");
+                    Thread.Sleep(100);
+                    Console.Write("\n");
+                }
+
+            }
+
+            for (int b = 0; b < map.GetLength(1) * scale + 2; b++)
+            {
+                if (b == 0)
+                {
+                    Console.Write("╚");
+                }
+                else if (b == map.GetLength(1) * scale + 1)
+                {
+                    Console.Write("╝");
+                }
+                else if (b > map.GetLength(1) * scale + 1)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("═");
+                }
+
+
+            }
+        }
+
         static void HUD()
         {
             Console.SetCursorPosition(65, 0);
             Console.Write("HUD");
 
-            Console.SetCursorPosition(60, 1);
-            Console.Write("  ~~~~~~~~~~");
+            Console.SetCursorPosition(62, 1);
+            Console.Write("~~~~~~~~~~");
+            Console.SetCursorPosition(60, 2);
+            Console.Write($"Player Health: {playerHealth}");
+            Console.SetCursorPosition(60, 3);
+            Console.Write($"Staus: {playerStatus}");
+            Console.SetCursorPosition(60, 4);
+            Console.Write($"Total Gold: {curGold}");
+            Console.SetCursorPosition(60, 5);
+            Console.Write($"Total enemies killed: {totalEnemyKilled}");
+
+            Console.SetCursorPosition(62, 6);
+            Console.Write("~~~~~~~~~~");
+            Console.SetCursorPosition(60, 7);
+            Console.Write($"Total Enemies on map: {Enemies.Count}");
+
+
+        }
+
+        static void HealthStatus()
+        {
+            if (playerHealth == 100)
+            {
+                playerStatus = "Perfectly healthy";
+            }
+            else if (playerHealth >= 90 && playerHealth < 100)
+            {
+                playerStatus = "Very Healthy";
+            }
+            else if (playerHealth >= 75 && playerHealth < 100)
+            {
+                playerStatus = "Healthy";
+            }
+            else if (playerHealth >= 50 && playerHealth < 100)
+            {
+                playerStatus = "Hurt";
+            }
+            else if (playerHealth >= 25 && playerHealth < 100)
+            {
+                playerStatus = "Badly hurt";
+            }
+            else if (playerHealth >= 10 && playerHealth < 100)
+            {
+                playerStatus = "Danger";
+            }
+            else if (playerHealth > 0 && playerHealth < 100)
+            {
+                playerStatus = "Immedate Danger";
+            }
+            else if(playerHealth == 0 && playerHealth < 100)
+            {
+                playerStatus = "dead";
+            }
+            else
+            {
+                playerStatus = "not valid health status";
+            }
         }
         //Actual Game loop logic
+        static void ProcessInput()
+        {
+
+        }
+        static void Update()
+        {
+            HealthStatus();
+        }
         static void Draw()
         {
             MakeMap(2);
             HUD();
+
+            Console.SetCursorPosition(xAxisPlayer, yAxisPlayer);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("@");
+            Console.ResetColor();
         }
+
+        
 
         
     }
